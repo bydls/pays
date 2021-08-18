346429238389ab0b0a7e019953d44310af40bfee
bydls <944870269@qq.com>
2021-08-18 13:20:11 +0800

    修复支付宝bug

--- src/Pay/GateWays/Ali/Support.php
+++ src/Pay/GateWays/Ali/Support.php
@@ -3,6 +3,7 @@
 namespace bydls\pays\Pay\GateWays\Ali;
 
 use Exception;
+use bydls\pays\Traits\HasHttpRequest;
 use bydls\pays\Pay\Events;
 use bydls\pays\Pay\Exceptions;
 use bydls\pays\Pay\GateWays\Ali;
@@ -10,7 +11,6 @@ use bydls\pays\Log\Log;
 use bydls\Support\Collection;
 use bydls\pays\Pay\Config\Config;
 use bydls\Support\Str;
-use bydls\pays\Traits\HasHttpRequest;
 
 /**
  *
